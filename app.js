af3b20aea5e302c2e0ca03a02fd5db112c4add30
Joshua Makula <makulajosh@gmail.com>
2023-03-10 13:37:48 +0300

    render HTML data using Node.js

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const port = 3000
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write('Hello World')
+
+    fs.readFile('index.html', function(error, data){
+        if(error) {
+            res.writeHead(404)
+            res.write('Error: File no found')
+        } else {
+            res.write(data)
+        }
     res.end()
+    })
 })
 
 server.listen(port, function(error){
